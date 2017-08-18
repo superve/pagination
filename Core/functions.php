b7a4f5c76d2e3a435173d6613e0734e182d504d7
gogery <gogery@163.com>
2017-08-18 22:25:14 +0800

    no message

--- Core/functions.php
+++ Core/functions.php
@@ -3,7 +3,7 @@
 	function dhalt($data)
 	{
 		header('Content-Type:text/html;charset=utf-8');
-		echo "<pre>" . print_r($data, true) . "</pre>";
+		echo "<pre>" . print_r($data, true) . "</pre>";die();
 	}
 
  ?>
